feaa99ba4f01320138545f3cf5d6debb1d2a669f
changhyeon <ckd0523@naver.com>
2024-04-12 17:46:07 +0900

    24-04-12

--- src/main/java/com/codehows/springbootdeveloper/DAO/Member.java
+++ src/main/java/com/codehows/springbootdeveloper/DAO/Member.java
@@ -1,10 +1,7 @@
 package com.codehows.springbootdeveloper.DAO;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
